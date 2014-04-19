8ff13533a0c4cd7167ef8ba5878a1824c12313b2
Luke Kuza <luke.kuza@allstarwireless.com>
2014-04-19 14:45:26 -0400

    Update code to use Jsoup and add ability for new MiStar system

--- src/com/darkscripting/zangle/constants/ZangleConstants.java
+++ src/com/darkscripting/zangle/constants/ZangleConstants.java
@@ -11,44 +11,6 @@ public interface ZangleConstants {
     //Parsing Constants
 
     /**
-     * Line to search for when finding students name
-     */
-    final String USERNAME_HTML_PARSE = "<td align=\"left\" valign=\"middle\">";
-    /**
-     * Line to search for when finding users school
-     */
-    final String SCHOOL_HTML_PARSE = "Membership:";
-    /**
-     * Line to search for when finding the students grade
-     */
-    final String GRADE_HTML_PARSE = "<td align=\"center\" valign=\"middle\" bgcolor=\"#6666CC\" class=\"stubanner\">";
-    /**
-     * Line to search for when finding the students period and all other class information
-     */
-    final String CLASSES_HTML_PARSE = "Period:";
-    //final String CLASS_HTML_PARSE = "&nbsp;&nbsp;";
-
-    /**
-     * Line to search for when the end of the assignment.aspx file is reached and we may finish parsing
-     */
-    final String END_OF_CLASSES_HTML_PARSE = "<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">";
-
-    /**
-     * Line to search for when we are finding a new assignment
-     */
-    final String ASSIGNMENT_HTML_PARSE = "<input type=\"hidden\"";
-
-    /**
-     * Line to search for to find if assignment is extra credit/not graded
-     */
-    final String ASSIGNMENT_EXTRA_CREDIT_NOT_GRADED_PARSE = "<img src=\"images/check3.gif\" height=\"15\" border=\"0\">";
-
-    /**
-     * Line to search to find if assignment has details  Line 34
-     */
-    final String ASSIGNMENT_DETAILS = "title=\"Show Assignment Detail\"></a>";
-
-    /**
      * Line to search for when the line contains a description
      */
     final String ASSIGNMENT_DETAILS_PAGE = "<td valign=\"top\"";
@@ -87,17 +49,9 @@ public interface ZangleConstants {
      */
     final String LOGIN_CHECK_EXTENSION = "Home/Login";
     /**
-     * Student demographics page.  Contains Student information
-     */
-    final String STUDENT_DEM_EXTENSION = "studemographics.aspx";
-    /**
      * Student Classes and Assignment page.  Contains ALL assignments
      */
     final String STUDENT_ASSIGN_EXTENSION = "Home/LoadProfileData/Assignments%5Etrue";
-    /**
-     * Student Classes and Assignments page. Contains recent assignments
-     */
-    final String STUDENT_RECENTASSIGN_EXTENSION = "stuassignments.aspx";
     /**
      * Assignments page.  Contains the assignment description(if any)
      */
